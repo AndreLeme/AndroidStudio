84e1053222bd36131752f8584e14264d5cf8bd0f
AndreLeme <62617416+AndreLeme@users.noreply.github.com>
2020-08-22 01:01:40 -0300

    lesson 4, 9

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -6,7 +6,8 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int scoreA;
+    int scoreB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,24 +23,66 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Displays the given score for 3 points.
+     * Increases the given score for 3 points.
      */
     public void threePointsA(View v) {
-        displayForTeamA(3);
+        scoreA = scoreA + 3;
+        displayForTeamA(scoreA);
     }
 
     /**
-     * Displays the given score for 2 points.
+     * Increases the given score for 2 points.
      */
     public void twoPointsA(View v) {
-        displayForTeamA(2);
+        scoreA = scoreA + 2;
+        displayForTeamA(scoreA);
     }
 
     /**
-     * Displays the given score for 1 point.
+     * Increases the given score for 1 point.
      */
     public void freeThrowA(View v) {
-        displayForTeamA(1);
+        scoreA = scoreA + 1;
+        displayForTeamA(scoreA);
     }
 
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Increases the given score for 3 points.
+     */
+    public void threePointsB(View v) {
+        scoreB = scoreB + 3;
+        displayForTeamB(scoreB);
+
+    }
+
+    /**
+     * Increases the given score for 2 points.
+     */
+    public void twoPointsB(View v) {
+        scoreB = scoreB + 2;
+        displayForTeamB(scoreB);
+    }
+
+    /**
+     * Increases the given score for 1 point.
+     */
+    public void freeThrowB(View v) {
+        scoreB = scoreB + 1;
+        displayForTeamB(scoreB);
+    }
+
+    public void reset(View v) {
+        scoreA = 0;
+        scoreB = 0;
+        displayForTeamA(scoreA);
+        displayForTeamB(scoreB);
+    }
 }
--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -34,22 +34,29 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = quantity * 5;
+        int price = calculatePrice();
         // String priceMessage = "Amount due $" + price;
         // String priceMessage = "That would be $" + price + " please";
         // String priceMessage = "You owe " + price + " bucks, dude!";
         // String priceMessage = price + " dollars for " + quantity + " cups of coffee. Pay up";
-        String priceMessage = "Total= $" + price;
-        priceMessage = priceMessage + "\nThank you!";
-        displayMessage(priceMessage);
+        /**
+        * String priceMessage = "Total= $" + price;
+        * priceMessage = priceMessage + "\nThank you!";
+        * displayMessage(priceMessage);
+        **/
+        displayMessage(createOrderSummary(price));
+
+        //calculatePrice(quantity);
+        //calculatePrice(quantity, 10);
+        //calculatePrice();
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int numbered) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + numbered);
     }
 
     /**
@@ -65,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void increment(View view) {
         quantity = quantity + 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
@@ -73,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void decrement(View view) {
         quantity = quantity - 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
@@ -84,4 +91,26 @@ public class MainActivity extends AppCompatActivity {
         priceTextView.setText(message);
     }
 
+    /**
+     * Calculates the price of the order.
+     * @return total price
+     */
+    private int calculatePrice() {
+        return quantity * 5;
+    }
+
+    /**
+     * Creates order summary.
+     *
+     * @param price of the order
+     * @return text summary
+     */
+    private String createOrderSummary(int price) {
+        //String msg = "Name: Duh Durd" + "\nQuantity: " + quantity + "\nTotal: $" + calculatePrice() + "\nThank you!";
+        String msg = "Name: Duh Durd";
+        msg += "\nQuantity: " + quantity;
+        msg += "\nTotal: $" + calculatePrice();
+        msg += "\nThank you!";
+        return msg;
+    }
 }
