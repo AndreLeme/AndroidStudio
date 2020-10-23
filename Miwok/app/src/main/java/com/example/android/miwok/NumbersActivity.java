9bfca67c28e689830a5b846adc16c3243fffcd5c
AndreLeme <62617416+AndreLeme@users.noreply.github.com>
2020-10-23 20:13:56 -0300

    multiscreen lesson 5, 10

--- Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,12 +3,19 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
+    private MediaPlayer mMediaPlayer;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +38,14 @@ public class NumbersActivity extends AppCompatActivity {
         ListView listView = findViewById(R.id.list);
 
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                mMediaPlayer = MediaPlayer.create(NumbersActivity.this, R.raw.number_one);
+                mMediaPlayer.start();
+            }
+        });
 
     }
 }
